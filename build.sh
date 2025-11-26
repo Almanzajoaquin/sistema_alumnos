32625fbcc2528d13452a8d875b6a211d048b476c
joaquin <almanzajoaquin25@gmail.com>
2025-11-26 16:46:48 -0300

    I NEED A HERO

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# FORZAR migraciones de todas las apps
+python manage.py makemigrations usuarios
+python manage.py makemigrations alumnos
+python manage.py makemigrations scraper
+python manage.py makemigrations
 python manage.py migrate
